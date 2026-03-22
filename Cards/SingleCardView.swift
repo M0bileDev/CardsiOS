42a34c17b7f94c7db14c34343c2a1b5fb326780e
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-22 20:40:33 +0100

    feature(main): create single card view

---
+++ Cards/SingleCardView.swift
@@ -0,0 +1,18 @@
+//
+//  SingleCardView.swift
+//  Cards
+//
+//  Created by Damian Ogórek on 22/03/2026.
+//
+
+import SwiftUI
+
+struct SingleCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SingleCardView()
+}
