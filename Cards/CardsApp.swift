c53f3d1fbddfade0bcee90beafe577accc6c897c
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-22 20:22:34 +0100

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Damian Ogórek on 22/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
