1291f92004aeddb8bf2f51e5ec3987874c81447c
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-22 20:58:13 +0100

    feature(main): add tollbar with toolbar item at the top right of the screen

--- Cards/SingleCardView.swift
+++ Cards/SingleCardView.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 
 struct SingleCardView: View {
+
+    @Environment(\.dismiss) var dismiss
+
     var body: some View {
         Color.yellow
+            .toolbar(content: {
+                ToolbarItem(
+                    placement: .topBarTrailing,
+                    content: {
+                        Button("Done") {
+                            dismiss()
+                        }
+                    }
+                )
+            })
     }
 }
 
