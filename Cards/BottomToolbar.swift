b3a8f263862c6185ec5be28e99ade04c5c4648ce
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-22 21:08:00 +0100

    feature(main): cretae view that represents bottom bar

---
+++ Cards/BottomToolbar.swift
@@ -0,0 +1,18 @@
+//
+//  BottomToolbar.swift
+//  Cards
+//
+//  Created by Damian Ogórek on 22/03/2026.
+//
+
+import SwiftUI
+
+struct BottomToolbar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BottomToolbar()
+}
