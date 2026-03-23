86642f053c95c54340099a9768eed9f96d19466d
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-23 22:26:50 +0100

    reafactor(main): refactor body by conditional logic

--- Cards/BottomToolbar.swift
+++ Cards/BottomToolbar.swift
@@ -8,39 +8,44 @@
 import SwiftUI
 
 struct BottomToolbar: View {
-    
+
     @Binding var modal: ToolbarSelection?
-    
-    
+
     var body: some View {
-        HStack{
-            Button(action: {
-                modal = .stickerModal
-            }, label: {
-                ToolbarButton()
-            })
+        HStack {
+            Button(
+                action: {
+                    modal = .stickerModal
+                },
+                label: {
+                    ToolbarButton()
+                }
+            )
         }
     }
 }
 
 struct ToolbarButton: View {
-    private let modalButton : [
-        ToolbarSelection: (text:String, imageName:String)
-    ] = [
-        .photoModal: ("Photos","photo"),
-        .frameModal: ("Frames","square.on.circle"),
-        .stickerModal: ("Stickers","heart.circle"),
-        .textModal: ("Text","textFormat")
-    ]
+    private let modalButton:
+        [ToolbarSelection: (text: String, imageName: String)] = [
+            .photoModal: ("Photos", "photo"),
+            .frameModal: ("Frames", "square.on.circle"),
+            .stickerModal: ("Stickers", "heart.circle"),
+            .textModal: ("Text", "textFormat"),
+        ]
     let modal: ToolbarSelection
-    
+
     var body: some View {
-        VStack {
-            Image(systemName: "heart.circle")
-                .font(.largeTitle)
-            Text("Stickers")
+        if let text = modalButton[modal]?.text,
+            let imageName = modalButton[modal]?.imageName
+        {
+            VStack {
+                Image(systemName: imageName)
+                    .font(.largeTitle)
+                Text(text)
+            }
+            .padding(.top)
         }
-        .padding(.top)
     }
 }
 
