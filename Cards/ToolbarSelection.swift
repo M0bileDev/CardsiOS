ec49775e7b0178e9d04c58b47f161bc941afdd8d
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-23 22:45:12 +0100

    feature(main): provide property that conforms to Identifiable protocol

--- Cards/ToolbarSelection.swift
+++ Cards/ToolbarSelection.swift
@@ -5,6 +5,7 @@
 //  Created by Damian Ogórek on 22/03/2026.
 //
 
-enum ToolbarSelection : CaseIterable {
+enum ToolbarSelection : CaseIterable, Identifiable {
+    var id = UUID()
     case photoModal, frameModal, stickerModal, textModal
 }
