b30a31023251b4c323829b0f877326d2ad10e27c
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-30 22:49:20 +0200

    feature(models): create model that holds infomration about transform state

---
+++ Cards/Models/Transform.swift
@@ -0,0 +1,14 @@
+//
+//  Transform.swift
+//  Cards
+//
+//  Created by Damian Ogórek on 30/03/2026.
+//
+
+import SwiftUI
+
+struct Transform {
+    var size = CGSize(width: 250, height: 180)
+    var rotation: Angle = .zero
+    var offset: CGSize = .zero
+}
