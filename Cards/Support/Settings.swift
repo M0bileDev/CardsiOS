b3f2809572c87821a6207f9c8338234797acf213
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-01 22:43:21 +0200

    refactor(support): change type to provide one copy of Settings

--- Cards/Support/Settings.swift
+++ Cards/Support/Settings.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct Settings {
+enum Settings {
     static let cardSize = CGSize(width: 1300, height: 2000)
     static let thumbnailSize = CGSize(width: 150, height: 250)
     static let defaultElementSize = CGSize(width: 250, height: 180)
