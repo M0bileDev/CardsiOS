cf3eb9cf7bdcfc04c4fbbf6a63968f9ed3541329
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-11 21:12:49 +0200

    feature(views): create view for image element

--- Cards/Views/CardElementView.swift
+++ Cards/Views/CardElementView.swift
@@ -9,10 +9,22 @@ import SwiftUI
 
 struct CardElementView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text( /*@START_MENU_TOKEN@*/"Hello, World!" /*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
     CardElementView()
+}
+
+struct ImageElementView: View {
+
+    let element: ImageElement
+
+    var body: some View {
+        element
+            .image
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+    }
 }
