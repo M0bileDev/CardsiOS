f6472f0ddcbc248a7d23fc5819af63d094a706a3
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-16 23:01:07 +0200

    feature(views): implement body that displays PhotoPicker to select photos

--- Cards/Views/Card Modal Views/PhotosModal.swift
+++ Cards/Views/Card Modal Views/PhotosModal.swift
@@ -5,15 +5,29 @@
 //  Created by Damian Ogórek on 16/04/2026.
 //
 
-import SwiftUI
 import PhotosUI
+import SwiftUI
 
 struct PhotosModal: View {
+
+    @Binding var card: Card
+    //an identifier and the type of content -> like .jpeg
+    @State private var selectedPhotos: [PhotosPickerItem] = []
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        PhotosPicker(
+            //when selected will be added to array
+            selection: $selectedPhotos,
+            //the filter for images
+            matching: .images,
+            
+            label: {
+                ToolbarButton(modal: .photoModal)
+            }
+        )
     }
 }
 
 #Preview {
-    PhotosModal()
+    PhotosModal(card: .constant(Card()))
 }
