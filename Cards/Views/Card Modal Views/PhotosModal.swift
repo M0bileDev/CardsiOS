21b14ee929c47bcaf946887dacc0fd605e7be08e
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-16 22:49:06 +0200

    feature(views): create modal view that represents PhotosUI framework with photos assets

---
+++ Cards/Views/Card Modal Views/PhotosModal.swift
@@ -0,0 +1,19 @@
+//
+//  PhotosModal.swift
+//  Cards
+//
+//  Created by Damian Ogórek on 16/04/2026.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct PhotosModal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PhotosModal()
+}
