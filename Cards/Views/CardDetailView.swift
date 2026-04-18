1f2bbe493cca137e8f2bc11485b483aecef4dd43
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-18 21:35:57 +0200

    feature(views): add modifier that receives images drag-and-dropped from different apps (e.g. Safari browser)

--- Cards/Views/CardDetailView.swift
+++ Cards/Views/CardDetailView.swift
@@ -28,6 +28,17 @@ struct CardDetailView: View {
                 }
             )
         }
+        .dropDestination(for: Data.self) {
+            receivedData,
+            location in
+            print(location)
+            for data in receivedData {
+                if let image = UIImage(data: data) {
+                    card.addElement(uiImage: image)
+                }
+            }
+            return !receivedData.isEmpty
+        }
     }
 }
 
