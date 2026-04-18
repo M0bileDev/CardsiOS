c6bd82ebd92cd1fa306563774130468f9853d9c7
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-18 22:02:18 +0200

    feature(models): create function that supports transferable elements

--- Cards/Models/Card.swift
+++ Cards/Models/Card.swift
@@ -11,15 +11,23 @@ struct Card: Identifiable {
     let id = UUID()
     var backgroundColor: Color = .yellow
     var elements: [CardElement] = []
-    
-    mutating func addElement(uiImage: UIImage){
+
+    mutating func addElement(uiImage: UIImage) {
         let element = ImageElement(uiImage: uiImage)
         elements.append(element)
     }
-    
-    mutating func addElement(text: TextElement){
+
+    mutating func addElement(text: TextElement) {
         elements.append(text)
     }
-    
-    
+
+    mutating func addElements(from transfer: [CustomTransfer]) {
+        for element in transfer {
+            if let text = element.text {
+                addElement(text: TextElement(text: text))
+            } else if let image = element.image {
+                addElement(uiImage: image)
+            }
+        }
+    }
 }
