fa2ce91eaa844037d4455a25c1709ddf3441ba5c
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-22 21:43:09 +0200

    feature(models): create file that contains shapes
    Created preview of built-in-shapes

---
+++ Cards/Models/Shapes.swift
@@ -0,0 +1,21 @@
+//
+//  Shapes.swift
+//  Cards
+//
+//  Created by Damian Ogórek on 22/04/2026.
+//
+
+import SwiftUI
+
+
+
+#Preview {
+    VStack{
+        Rectangle()
+        RoundedRectangle(cornerRadius: 25.0)
+        Circle()
+        Capsule()
+        Ellipse()
+    }
+    .padding()
+}
