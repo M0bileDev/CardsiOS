28d32bc77265e92b806a762b5122e588178d6408
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-24 22:01:46 +0200

    feature(views): create frame modal that repressents list of shapes

---
+++ Cards/Views/Card Modal Views/FrameModal.swift
@@ -0,0 +1,18 @@
+//
+//  FrameModal.swift
+//  Cards
+//
+//  Created by Damian Ogórek on 24/04/2026.
+//
+
+import SwiftUI
+
+struct FrameModal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameModal()
+}
