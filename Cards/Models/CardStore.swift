2f4d1f361a6f1e806f0dd699e356f435a835313b
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-24 21:34:19 +0200

    feature(models): add observable published property (for selected element)

--- Cards/Models/CardStore.swift
+++ Cards/Models/CardStore.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 class CardStore: ObservableObject {
     @Published var cards: [Card] = []
+    @Published var selectedElement: CardElement?
 
     init(defaultData: Bool = false) {
         if defaultData {
