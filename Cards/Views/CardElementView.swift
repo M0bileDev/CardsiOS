2137e6b2949a90591851424806160d7d86b1b28b
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-25 22:39:23 +0200

    feature(views): create extension for ImageElementView that conditionally apply clip with shape if frameIndex is not nil

--- Cards/Views/CardElementView.swift
+++ Cards/Views/CardElementView.swift
@@ -13,7 +13,7 @@ struct CardElementView: View {
 
     var body: some View {
         if let element = element as? ImageElement {
-            ImageElementView(element: element)
+            ImageElementView(element: element).clip()
         }
         if let element = element as? TextElement {
             TextElementView(element: element)
@@ -30,6 +30,7 @@ struct ImageElementView: View {
             .image
             .resizable()
             .aspectRatio(contentMode: .fit)
+            
     }
 }
 
@@ -43,6 +44,19 @@ struct TextElementView: View {
                 .font(.custom(element.textFont, size: 200))
                 .foregroundStyle(element.textColor)
                 .scalableText()
+        }
+    }
+}
+
+private extension ImageElementView {
+
+    @ViewBuilder
+    func clip() -> some View {
+        if let frameIndex = element.frameIndex {
+            let shape = Shapes.shapes[frameIndex]
+            self.clipShape(shape)
+        } else {
+            self
         }
     }
 }
