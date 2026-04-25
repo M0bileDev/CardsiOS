4d09081a36b022b11b88c740f66b00faac65bffb
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-25 22:23:32 +0200

    refactor(preview_data): update empty array of preview data

--- Cards/Preview Content/PreviewData.swift
+++ Cards/Preview Content/PreviewData.swift
@@ -15,6 +15,26 @@ var initialCards: [Card] { [
   Card(backgroundColor: .yellow)
 ] }
 
-var initialElements: [CardElement] {
-    []
-}
+var initialElements: [CardElement] { [
+  ImageElement(
+    transform: Transform(
+      size: CGSize(width: 250, height: 180),
+      offset: CGSize(width: 27, height: -140)),
+    uiImage: UIImage(named: "hedgehog1")),
+  ImageElement(
+    transform: Transform(
+      size: CGSize(width: 380, height: 270),
+      offset: CGSize(width: -80, height: 25)),
+    uiImage: UIImage(named: "hedgehog2")),
+  ImageElement(
+    transform: Transform(
+      size: CGSize(width: 250, height: 180),
+      offset: CGSize(width: 80, height: 205)),
+    uiImage: UIImage(named: "hedgehog3")),
+  TextElement(
+    transform: Transform(
+      size: Settings.defaultElementSize * 1.1,
+      offset: CGSize(width: 10, height: -275)),
+    text: "Hedgehogs!!!",
+    textColor: .black)
+] }
