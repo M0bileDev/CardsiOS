cc8ad4dbbf86233a583aa23232f7c4ded12f9168
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-26 21:28:41 +0200

    feature(extensions): create function that conditionally resize large image

--- Cards/Extensions/UIImageExtensions.swift
+++ Cards/Extensions/UIImageExtensions.swift
@@ -9,6 +9,26 @@ import SwiftUI
 
 extension UIImage {
 
+    func resizeLargeImage() -> UIImage {
+        let defaultSize: CGFloat = 1000
+        if size.width <= defaultSize || size.height <= defaultSize {
+            return self
+        }
+
+        let scale: CGFloat
+        if size.width >= size.height {
+            scale = defaultSize / size.width
+        } else {
+            scale = defaultSize / size.height
+        }
+
+        let newSize = CGSize(
+            width: size.width * scale,
+            height: size.height * scale
+        )
+        return resize(to: newSize)
+    }
+
     func resize(to size: CGSize) -> UIImage {
         let imageRendererFormat = UIGraphicsImageRendererFormat()
         //100×100 means 100×100 pixels, no multiplier
