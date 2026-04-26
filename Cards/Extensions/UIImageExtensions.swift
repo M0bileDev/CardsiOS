24b24af14f787225ff4b9f81f51e08044fe27de5
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-26 21:20:44 +0200

    feature(extensions): create extension function that set renderer scale to 1 in UIImage and  resize UIImage for exact pixel output

--- Cards/Extensions/UIImageExtensions.swift
+++ Cards/Extensions/UIImageExtensions.swift
@@ -5,3 +5,17 @@
 //  Created by Damian Ogórek on 26/04/2026.
 //
 
+import SwiftUI
+
+extension UIImage {
+
+    func resize(to size: CGSize) -> UIImage {
+        let imageRendererFormat = UIGraphicsImageRendererFormat()
+        //100×100 means 100×100 pixels, no multiplier
+        imageRendererFormat.scale = 1
+        return UIGraphicsImageRenderer(size: size, format: imageRendererFormat)
+            .image(actions: { _ in
+                draw(in: CGRect(origin: .zero, size: size))
+            })
+    }
+}
