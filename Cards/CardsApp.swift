243494885fd83c6a458ce4be03cc8727e248873c
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-28 22:02:50 +0200

    refactor(cards): remove test model

--- Cards/CardsApp.swift
+++ Cards/CardsApp.swift
@@ -10,10 +10,6 @@ import SwiftUI
 @main
 struct CardsApp: App {
 
-    init() {
-        Team.load()
-    }
-
     @StateObject var store = CardStore(defaultData: true)
 
     var body: some Scene {
@@ -23,37 +19,3 @@ struct CardsApp: App {
         }
     }
 }
-
-struct Team: Codable {
-    let names: [String]
-    let count: Int
-
-    static func save() {
-        do {
-            let encoder = JSONEncoder()
-            encoder.outputFormatting = .prettyPrinted
-            let data = try encoder.encode(teamData)
-            let url = URL.documentsDirectory.appendingPathComponent("TeamData")
-            try data.write(to: url)
-        } catch {
-            print(error.localizedDescription)
-        }
-    }
-
-    static func load() {
-        let url = URL.documentsDirectory.appendingPathComponent("TeamData")
-        do {
-            let data = try Data(contentsOf: url)
-            let decoder = JSONDecoder()
-            let team = try decoder.decode(Team.self, from: data)
-            print(team)
-        } catch {
-            print(error.localizedDescription)
-        }
-    }
-}
-
-let teamData = Team(
-    names: ["John", "Rachel", "Anna", "Jake"],
-    count: 4
-)
