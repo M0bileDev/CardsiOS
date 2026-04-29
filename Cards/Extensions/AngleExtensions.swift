e939f63ec45c01e2d6c9ff7c50bb4e111f7123f9
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-29 21:58:11 +0200

    refactor(extensions) rename enum

--- Cards/Extensions/AngleExtensions.swift
+++ Cards/Extensions/AngleExtensions.swift
@@ -10,18 +10,18 @@ import SwiftUI
 extension Angle: Codable {
     public init(from decoder: Decoder) throws {
         //configure container to decode the data, same as encode
-        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let container = try decoder.container(keyedBy: AngleCodingKeys.self)
         let degrees = try container.decode(Double.self, forKey: .degrees)
         self.init(degrees: degrees)
     }
 
     public func encode(to encoder: Encoder) throws {
         //configure encoder by set its container
-        var container = encoder.container(keyedBy: CodingKeys.self)
+        var container = encoder.container(keyedBy: AngleCodingKeys.self)
         try container.encode(degrees, forKey: .degrees)
     }
 }
 
-enum CodingKeys: CodingKey {
+enum AngleCodingKeys: CodingKey {
     case degrees
 }
