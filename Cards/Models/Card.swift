6cb97d391a65ac968085b1a160cf2895475ef179
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-29 21:44:35 +0200

    refactor(models): update logic that removes file from disk if the element is ImageElement

--- Cards/Models/Card.swift
+++ Cards/Models/Card.swift
@@ -37,6 +37,10 @@ struct Card: Identifiable {
     }
 
     mutating func remove(_ element: CardElement) {
+        if let element = element as? ImageElement {
+            UIImage.remove(name: element.imageFilename)
+        }
+
         if let index = element.index(in: elements) {
             elements.remove(at: index)
         }
