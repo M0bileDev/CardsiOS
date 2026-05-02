3a5f363e0ac4f4f8169bbd43f63df9754c5a2f80
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-02 22:34:30 +0200

    feature(models): create function that logic is to create new card, append it to array of cards and save it to documents folder

--- Cards/Models/CardStore.swift
+++ Cards/Models/CardStore.swift
@@ -25,6 +25,13 @@ class CardStore: ObservableObject {
             cards.remove(at: index)
         }
     }
+
+    func addCard() -> Card {
+        let card = Card(backgroundColor: Color.random())
+        cards.append(card)
+        card.save()
+        return card
+    }
 }
 
 extension CardStore {
