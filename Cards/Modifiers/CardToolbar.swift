73131f993bae8ecb8b8044663e22fa65c296d4dd
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-02 23:15:28 +0200

    feature(views): add logic to display text modal when text has been selected from card toolbar

--- Cards/Modifiers/CardToolbar.swift
+++ Cards/Modifiers/CardToolbar.swift
@@ -15,6 +15,7 @@ struct CardToolbar: ViewModifier {
     @Binding var card: Card
     @State private var stickerImage: UIImage?
     @State private var frameIndex: Int?
+    @State private var textElement: TextElement = TextElement()
 
     var menu: some View {
         Menu(
@@ -78,6 +79,14 @@ struct CardToolbar: ViewModifier {
                                     )
                                 }
                                 frameIndex = nil
+                            })
+                    case .textModal:
+                        TextModal(textElement: $textElement)
+                            .onDisappear(perform: {
+                                if !textElement.text.isEmpty {
+                                    card.addElement(text: textElement)
+                                    textElement = TextElement()
+                                }
                             })
                     default: Text(String(describing: item))
                     }
