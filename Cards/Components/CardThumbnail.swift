43bb239f637e0a6211b1d0ec6fa51f0f47119dad
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-04 21:30:32 +0200

    refactor(components): move frame modifier from component to preview

--- Cards/Components/CardThumbnail.swift
+++ Cards/Components/CardThumbnail.swift
@@ -14,13 +14,14 @@ struct CardThumbnail: View {
     var body: some View {
         RoundedRectangle(cornerRadius: 15)
             .foregroundStyle(card.backgroundColor)
-            .frame(
-                width: Settings.thumbnailSize.width,
-                height: Settings.thumbnailSize.height
-            )
+
     }
 }
 
 #Preview {
     CardThumbnail(card: initialCards[0])
+        .frame(
+            width: Settings.thumbnailSize.width,
+            height: Settings.thumbnailSize.height
+        )
 }
