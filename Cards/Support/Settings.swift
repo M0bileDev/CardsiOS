e560951060146309aa55964c2f3932ac854ebe15
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-06 22:07:21 +0200

    feature(support): create function that calculates the size

--- Cards/Support/Settings.swift
+++ Cards/Support/Settings.swift
@@ -33,4 +33,19 @@ extension Settings {
 
         return offset
     }
+
+    static func calculateSize(_ size: CGSize) -> CGSize {
+        var newSize = size
+        let ratio = Settings.cardSize.width / Settings.cardSize.height
+
+        if size.width < size.height {
+            newSize.height = min(size.height, newSize.width / ratio)
+            newSize.width = min(size.width, newSize.height * ratio)
+        } else {
+            newSize.width = min(size.width, newSize.height * ratio)
+            newSize.height = min(size.height, newSize.width / ratio)
+        }
+
+        return newSize
+    }
 }
