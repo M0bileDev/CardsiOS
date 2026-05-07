fb3b5bb7395061bfac300cd7d81dadd9ef3b3c79
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-07 21:44:25 +0200

    feature(view): add overlay with clipping  that photo won’t overflow

--- Cards/Views/CardDetailView.swift
+++ Cards/Views/CardDetailView.swift
@@ -14,50 +14,52 @@ struct CardDetailView: View {
     var viewScale: CGFloat = 1.0
 
     var body: some View {
-        ZStack {
-            card.backgroundColor
-                .onTapGesture {
-                    store.selectedElement = nil
-                }
-            ForEach(
-                $card.elements,
-                id: \.id,
-                content: { $element in
-                    CardElementView(element: element)
-                        .overlay(
-                            element: element,
-                            isSelected: isSelected(element)
-                        )
-                        .onTapGesture {
-                            store.selectedElement = element
-                        }
-                        .elementContextMenu(card: $card, element: $element)
-                        .resizableView(
-                            transform: $element.transform,
-                            viewScale: viewScale
-                        )
-                        .frame(
-                            width: element.transform.size.width,
-                            height: element.transform.size.height
-                        )
 
-                }
-            )
-        }
-        .dropDestination(for: CustomTransfer.self) {
-            items,
-            location in
-            print(location)
-            Task {
-                await MainActor.run {
-                    card.addElements(from: items)
-                }
+        card.backgroundColor
+            .onTapGesture {
+                store.selectedElement = nil
             }
-            return !items.isEmpty
-        }
-        .onDisappear {
-            store.selectedElement = nil
-        }
+            .overlay {
+                ForEach(
+                    $card.elements,
+                    id: \.id,
+                    content: { $element in
+                        CardElementView(element: element)
+                            .overlay(
+                                element: element,
+                                isSelected: isSelected(element)
+                            )
+                            .onTapGesture {
+                                store.selectedElement = element
+                            }
+                            .elementContextMenu(card: $card, element: $element)
+                            .resizableView(
+                                transform: $element.transform,
+                                viewScale: viewScale
+                            )
+                            .frame(
+                                width: element.transform.size.width,
+                                height: element.transform.size.height
+                            )
+
+                    }
+                )
+            }
+            .clipped()
+            .dropDestination(for: CustomTransfer.self) {
+                items,
+                location in
+                print(location)
+                Task {
+                    await MainActor.run {
+                        card.addElements(from: items)
+                    }
+                }
+                return !items.isEmpty
+            }
+            .onDisappear {
+                store.selectedElement = nil
+            }
     }
 
     func isSelected(_ element: CardElement) -> Bool {
