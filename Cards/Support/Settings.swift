902924ce444626a9747da81d7e61bd6657c01d14
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-07 21:32:14 +0200

    feature(support): change default values

--- Cards/Support/Settings.swift
+++ Cards/Support/Settings.swift
@@ -10,7 +10,7 @@ import SwiftUI
 enum Settings {
     static let cardSize = CGSize(width: 1300, height: 2000)
     static let thumbnailSize = CGSize(width: 150, height: 250)
-    static let defaultElementSize = CGSize(width: 250, height: 180)
+    static let defaultElementSize = CGSize(width: 800, height: 800)
     static let borderColor: Color = .blue
     static let borderWidth: CGFloat = 5
 }
