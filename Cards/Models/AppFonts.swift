f08485ed50949fdee4402a58c5bb73fd7d9b9f7d
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-09 20:48:28 +0200

    feature(models): create enum that represents app fonts

---
+++ Cards/Models/AppFonts.swift
@@ -0,0 +1,30 @@
+//
+//  AppFonts.swift
+//  Cards
+//
+//  Created by Damian Ogórek on 09/05/2026.
+//
+
+import Foundation
+
+enum AppFonts {
+    static let fonts = [
+        "San Fransisco",
+        "AmericanTypewriter",
+        "Avenir-Heavy",
+        "Avenir-Book",
+        "Baskerville-Italic",
+        "ChalkboardSE-Regular",
+        "Chalkduster",
+        "Cochin-BoldItalic",
+        "Copperplate",
+        "GillSans-UltraBold",
+        "MarkerFelt-Wide",
+        "Verdana-Bold",
+        "Noteworthy-Bold",
+        "Papyrus",
+        "PartyLetPlain",
+        "SavoyeLetPlain",
+        "SnellRoundhand-Black",
+    ]
+}
