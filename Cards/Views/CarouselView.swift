f44a28998f9c36aabf21e4da9e2a18c20e50cb0f
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-09 21:11:46 +0200

    feature(views): create content

--- Cards/Views/CarouselView.swift
+++ Cards/Views/CarouselView.swift
@@ -14,7 +14,28 @@ struct CarouselView: View {
     let thumbnailScale: CGFloat = 0.7
 
     var body: some View {
-        Text( /*@START_MENU_TOKEN@*/"Hello, World!" /*@END_MENU_TOKEN@*/)
+        GeometryReader { proxy in
+            TabView(content: {
+                ForEach((0..<store.cards.count), id: \.self) { index in
+                    getOrCreateCardView(card: store.cards[index])
+                        .frame(
+                            width: getCardSize(proxySize: proxy.size).width,
+                            height: getCardSize(proxySize: proxy.size).height
+                        )
+                        .cornerRadius(15)
+                        .shadow(
+                            color: Color(white: 0.5, opacity: 0.7),
+                            radius: 5
+                        )
+                        .onTapGesture(perform: {
+                            selectedCard = store.cards[index]
+                        })
+                        .offset(y: -proxy.size.height * 0.05)
+                }
+            })
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+            .tabViewStyle(PageTabViewStyle())
+        }
     }
 
     func getCardSize(proxySize: CGSize) -> CGSize {
