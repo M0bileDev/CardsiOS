daf24b01a163467acdf13da6dc7b2aa2d129d11a
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-09 21:23:57 +0200

    feature(views): create property Card type, create function that returns list of scaled CardElementView

--- Cards/Views/ShareCardView.swift
+++ Cards/Views/ShareCardView.swift
@@ -8,11 +8,28 @@
 import SwiftUI
 
 struct ShareCardView: View {
+
+    let card: Card
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text( /*@START_MENU_TOKEN@*/"Hello, World!" /*@END_MENU_TOKEN@*/)
+    }
+
+    func scaledElements(size: CGSize) -> some View {
+        let viewScale = Settings.calculateScale(size)
+        return ForEach(card.elements, id: \.id) { element in
+            CardElementView(element: element)
+                .frame(
+                    width: element.transform.size.width,
+                    height: element.transform.size.height
+                )
+                .rotationEffect(element.transform.rotation)
+                .scaleEffect(viewScale)
+                .offset(element.transform.offset * viewScale)
+        }
     }
 }
 
-#Preview {
-    ShareCardView()
-}
+//#Preview {
+//    ShareCardView()
+//}
