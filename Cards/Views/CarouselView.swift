5ed9247b7a3b51e4692b0921a87209686879a50d
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-09 20:50:53 +0200

    feature(views): Create view that represents carousel

---
+++ Cards/Views/CarouselView.swift
@@ -0,0 +1,18 @@
+//
+//  CarouselView.swift
+//  Cards
+//
+//  Created by Damian Ogórek on 09/05/2026.
+//
+
+import SwiftUI
+
+struct CarouselView: View {
+    var body: some View {
+        Text( /*@START_MENU_TOKEN@*/"Hello, World!" /*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarouselView()
+}
