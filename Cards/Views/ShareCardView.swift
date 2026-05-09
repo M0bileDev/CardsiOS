8c9ae44f1552764a20d8ac952f5edc1481cf58e8
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-09 21:28:54 +0200

    feature(views): create function that returns card view with scaled elements

--- Cards/Views/ShareCardView.swift
+++ Cards/Views/ShareCardView.swift
@@ -15,6 +15,17 @@ struct ShareCardView: View {
         Text( /*@START_MENU_TOKEN@*/"Hello, World!" /*@END_MENU_TOKEN@*/)
     }
 
+    func scaledContent(size: CGSize) -> some View {
+        card.backgroundColor
+            .overlay(content: {
+                scaledElements(size: size)
+            })
+            .frame(
+                width: Settings.calculateSize(size).width,
+                height: Settings.calculateSize(size).height
+            )
+    }
+
     func scaledElements(size: CGSize) -> some View {
         let viewScale = Settings.calculateScale(size)
         return ForEach(card.elements, id: \.id) { element in
