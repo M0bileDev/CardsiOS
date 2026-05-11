f84253846dc8d6b4752bc10f44c159be8959220d
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-11 20:45:37 +0200

    feature(app_startup): show intermediate view which contains the splash

--- Cards/App Startup/CardsApp.swift
+++ Cards/App Startup/CardsApp.swift
@@ -14,7 +14,7 @@ struct CardsApp: App {
 
     var body: some Scene {
         WindowGroup {
-            CardsListView()
+            AppLoadingView()
                 .environmentObject(store)
         }
     }
