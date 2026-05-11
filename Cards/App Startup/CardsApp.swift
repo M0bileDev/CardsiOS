a4fd269da0237fd3421a7812067857742ab44edb
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-11 20:36:17 +0200

    refactor(app_startup): move root file into new folder

---
+++ Cards/App Startup/CardsApp.swift
@@ -0,0 +1,21 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Damian Ogórek on 22/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+
+    @StateObject var store = CardStore(defaultData: true)
+
+    var body: some Scene {
+        WindowGroup {
+            CardsListView()
+                .environmentObject(store)
+        }
+    }
+}
