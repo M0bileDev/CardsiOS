8dc81e85e6119eb4b61ee5b8a2eb7fc82a58b5ab
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-11 20:16:11 +0200

    feature(views): create color binding property
    Create computed property that returns color selector view

--- Cards/Views/TextView.swift
+++ Cards/Views/TextView.swift
@@ -8,8 +8,47 @@
 import SwiftUI
 
 struct TextView: View {
+
+    @Binding var color: Color
+
+    var colorSelector: some View {
+        ForEach(1..<8) { index in
+            let currentColor = Color("appColor\(index)")
+
+            ZStack {
+                Circle()
+                    .stroke(Color.white, lineWidth: 1.0)
+                    .overlay(content: {
+                        Circle()
+                            .foregroundStyle(
+                                color == currentColor ? currentColor : .white
+                            )
+                    })
+                    .frame(
+                        width: 44,
+                        height: 44
+                    )
+                Circle()
+                    .stroke(lineWidth: color == currentColor ? 0 : 1)
+                    .overlay(content: {
+                        Circle()
+                            .foregroundStyle(currentColor)
+                    })
+                    .frame(
+                        width: color == currentColor ? 50 : 40,
+                        height: color == currentColor ? 50 : 40
+                    )
+            }
+            .onTapGesture {
+                withAnimation {
+                    color = Color("appColor\(index)")
+                }
+            }
+        }
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text( /*@START_MENU_TOKEN@*/"Hello, World!" /*@END_MENU_TOKEN@*/)
     }
 }
 
