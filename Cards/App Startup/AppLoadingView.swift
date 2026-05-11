c1040f5e8376bac203628d86aad6a98767963778
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-11 20:38:44 +0200

    feature(app_startup): create view that represents animation if it will be loaded or not
    Created internal state property

---
+++ Cards/App Startup/AppLoadingView.swift
@@ -0,0 +1,21 @@
+//
+//  AppLoadingView.swift
+//  Cards
+//
+//  Created by Damian Ogórek on 11/05/2026.
+//
+
+import SwiftUI
+
+struct AppLoadingView: View {
+    
+    @State private var showSplash = true
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppLoadingView()
+}
