a47ba7b0db1bb1055cbaec0c548131b10b0fc2e1
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-11 20:40:34 +0200

    feature(app_startup): create view that represents spalsh screen

---
+++ Cards/App Startup/SplashScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen.swift
+//  Cards
+//
+//  Created by Damian Ogórek on 11/05/2026.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
